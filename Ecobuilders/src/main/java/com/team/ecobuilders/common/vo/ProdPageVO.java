0034b8c3ec20c096cb892f4c28f03dae7fcfcb01
kjoocheol <kjoocheol@gmail.com>
2024-10-18 17:52:50 +0900

    페이징

---
+++ Ecobuilders/src/main/java/com/team/ecobuilders/common/vo/ProdPageVO.java
@@ -0,0 +1,18 @@
+package com.team.ecobuilders.common.vo;
+
+public class ProdPageVO {
+	
+	
+	private int curPage = 1;
+	private int rowSizePerPage = 9;
+
+	private int firstRow;
+	private int lastRow;
+	private int totalPageCount;
+	private int firstPage;
+	private int lastPage;
+	
+	
+
+	
+}
