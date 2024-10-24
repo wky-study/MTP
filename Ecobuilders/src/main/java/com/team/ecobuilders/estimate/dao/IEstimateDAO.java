69af6afea88cd423c2f3ede7f91668efbadf78f5
qkranstn <godwooo7@naver.com>
2024-10-24 19:23:29 +0900

    10/24 19:23

--- Ecobuilders/src/main/java/com/team/ecobuilders/estimate/dao/IEstimateDAO.java
+++ Ecobuilders/src/main/java/com/team/ecobuilders/estimate/dao/IEstimateDAO.java
@@ -1,11 +1,12 @@
 package com.team.ecobuilders.estimate.dao;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Mapper;
 import com.team.ecobuilders.estimate.dto.EstimateDTO;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface IEstimateDAO {
-	List<EstimateDTO> getEstimateList();
-
+    List<EstimateDTO> getEstimateList(); // 견적서 목록 조회
+    void insertEstimate(EstimateDTO estimate); // 견적서 저장
+    EstimateDTO getEstimateById(String estId); // 견적서 ID로 조회
 }
