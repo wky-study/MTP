bf14923421d496707586f1fefd54cc709f4adb1f
offro <offro@DESKTOP-998EDS4>
2024-10-24 17:55:05 +0900

    마이페이지,견적서

--- Ecobuilders/src/main/java/com/team/ecobuilders/mypage/web/MypageController.java
+++ Ecobuilders/src/main/java/com/team/ecobuilders/mypage/web/MypageController.java
@@ -1,7 +1,9 @@
 package com.team.ecobuilders.mypage.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MypageController {
 
 	// 마이페이지 요청
@@ -13,4 +15,17 @@ public class MypageController {
 		// 마이페이지 화면을 응답시킴
 		return "KDH_member/mypageView";
 	}
+	
+	// 나의견적서 요청
+	@RequestMapping("/mp_estimateList")
+	public String my_estimateList() {
+
+		System.out.println("mp_estimateList 실행");
+		
+		// 마이페이지 화면을 응답시킴
+		return "KDH_member/mp_estimateList";
+	}
+	
+
+	
 }
